1bc97eb8ee56db387ebc35129cb26c7e21cce5e4
Vivek205 <n.vin95@gmail.com>
2019-06-10 17:38:04 +0530

    storybook: header and footer

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,6 +5,8 @@ import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
 import { Button, Welcome } from "@storybook/react/demo";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
 storiesOf("Welcome", module).add("to Storybook", () => (
   <Welcome showApp={linkTo("Button")} />
@@ -17,3 +19,7 @@ storiesOf("Button", module)
   .add("with some emoji", () => (
     <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
   ));
+
+storiesOf("Header", module).add("common header", () => <Header />);
+
+storiesOf("Footer", module).add("common footer", () => <Footer />);

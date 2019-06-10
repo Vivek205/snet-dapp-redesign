4071d094150548e5ce5f3e5b6c883b4d75d24205
dasari-ananya <ananya.dasari@nirman.io>
2019-06-10 13:02:31 +0530

    header and footer component created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import AiMarketplace from './components/AiMarketplace/index.js';
+
+class App extends Component {
+  render () {
+     return (
+        <AiMarketplace />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/AiMarketplace/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Header from '../common/Header/index.js';
+import Footer from '../common/Footer/index.js';
+
+class AiMarketplace extends Component {
+  render () {
+    return (
+    	<div>
+      	<Header />
+      	<Footer />
+      </div>
+    );
+  }
+}
+
+export default AiMarketplace;

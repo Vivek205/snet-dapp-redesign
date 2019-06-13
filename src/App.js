4c931ad8ea24b8f81002c70221349f8afec23006
Vivek205 <n.vin95@gmail.com>
2019-06-13 16:33:20 +0530

    aws-setup and react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Amplify from "aws-amplify";
+import { aws_config } from "./aws_config";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // components
-import AiMarketplace from './components/AiMarketplace/index.js';
+import AiMarketplace from "./components/AiMarketplace/index.js";
 // import Login from './components/Login/index.js';
-import SignUp from './components/Login/signup.js';
+import SignUp from "./components/Login/signup.js";
 // import ForgotPassword from './components/Login/forgotpassword.js';
 
 //  material ui theme
-import { ThemeProvider } from '@material-ui/styles';
+import { ThemeProvider } from "@material-ui/styles";
 
-import theme from './assets/theme.js';
+import theme from "./assets/theme.js";
 
-import './App.css';
+import "./App.css";
+
+Amplify.configure(aws_config);
 
 class App extends Component {
-  render () {
-     return (
-     	<ThemeProvider theme={theme}>
-      <SignUp />
-        	<AiMarketplace />
-        </ThemeProvider>
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/signup" component={SignUp} />
+            <Route path="/aimarketplace" component={AiMarketplace} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
